6807211b90867d75bac3f6ad7fa63ca5feeedffd
Master Of None <nezar.ras@gmail.com>
2022-07-21 08:15:24 +0300

    seperated model form trainer code

--- get_model.py
+++ get_model.py
@@ -8,28 +8,54 @@ from torch.autograd import Variable
 import torch.nn.functional as F
 # Keras
 import keras
+# Neural Network
+from model import Net
 
 def get_model(conf, layers_size=3,  load=False):
+
+    """
+    Generates a Neural Network and trains it on dataset
+
+    :param conf: preproccessed dataset configuration
+    :layers_size: amount of hidden layers
+    :return: a trained neural network
+    """
     assert(conf['Dataset'] == 'credit-g')
     assert(layers_size >= 2)
 
-    class Net(nn.Module):
-        def __init__(self, D_in, H, D_out, layers_size):
-            super(Net, self).__init__()
-            self.linears = nn.ModuleList([nn.Linear(D_in, H)])
-            self.linears.extend(nn.ModuleList([nn.Linear(H, H) for i in range(1,layers_size - 1)]))
-            self.linears.extend(nn.ModuleList([nn.Linear(H, D_out)]))
-            self.relu = nn.ReLU()
-            self.softmax = nn.Softmax(dim=0)
-            
-        def forward(self, x):
-            for linear in self.linears:
-                x = self.relu(linear(x))
-            y = self.softmax(x)
-            return y
-        
     def train(model, criterion, optimizer, X, y, N, n_classes):
-        pass
+        model.train()
+
+        current_loss = 0
+        current_correct = 0
+
+
+        # Training in batches
+        for ind in range(0, X.size(0), N):
+            indices = range(ind, min(ind + N, X.size(0)) - 1) 
+            inputs, labels = X[indices], y[indices]
+            inputs = Variable(inputs, requires_grad=True)
+
+
+            optimizer.zero_grad()
+
+            output = model(inputs)
+            _, indices = torch.max(output, 1) # argmax of output [[0.61, 0.12]] -> [0]
+            # [[0, 1, 1, 0, 1, 0, 0]] -> [[1, 0], [0, 1], [0, 1], [1, 0], [0, 1], [1, 0], [1, 0]]
+            preds = torch.tensor(keras.utils.to_categorical(indices, num_classes=n_classes))
+
+            loss = criterion(output, labels)
+            loss.backward()
+            optimizer.step()
+
+            current_loss += loss.item()
+            current_correct += (preds.int() == labels.int()).sum() /n_classes
+
+
+        current_loss = current_loss / X.size(0)
+        current_correct = current_correct.double() / X.size(0)    
+
+        return preds, current_loss, current_correct.item()
     
     df = conf['TrainData']
     target = conf['Target']
